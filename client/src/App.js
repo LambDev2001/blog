2ec4057dd833133b8b7452189bbe257b4770d4dd
lam <LambDev2001@gmail.com>
2023-08-21 16:04:15 +0700

    client init and setup store with UI libary

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div class="d-flex flex-column mb-3">
+  <div class="d-flex flex-end">Flex item 1</div>
+  <div class="p-2">Flex item 2</div>
+  <div class="p-2">Flex item 3</div>
+</div>
+  );
+}
+
+export default App;
